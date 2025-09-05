c7d8e3cb48fa0c52863c6eb24a86bcd2568e966e
Luutyy <lautaramirezlopez@gmail.com>
2025-09-05 12:44:58 -0300

    Implementacion de la clase Cell

---
+++ src/Program/Cell.cs
@@ -0,0 +1,23 @@
+using System.Net.Http.Headers;
+
+namespace Ucu.Poo.GameOfLife;
+
+public class Cell
+{
+    public bool Alive { get; set; }
+
+    public Cell()
+    {
+        Alive = false;
+    }
+    
+    public void Born()
+    {
+        Alive = true;
+    }
+
+    public void Die()
+    {
+        Alive = false;
+    }
+}
